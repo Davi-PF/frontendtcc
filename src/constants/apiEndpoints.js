40524ba0713ab9680336d52c61cff0ee4d78fd04
Davi Prudente Ferreira <94153717+Davi-PF@users.noreply.github.com>
2024-12-08 20:46:33 -0300

    Pequena alteração

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,5 +1,5 @@
 const API_URL = "http://zlo-main-app.us-east-1.elasticbeanstalk.com"
-// const API_URL = "http://localhost:5000/"
+//const API_URL = "http://localhost:5000"
 export const API_ENCRYPT = API_URL + "/api/url/encrypt"
 export const API_DECRYPT = API_URL + "/api/url/decrypt"
 export const API_VERIFY_DEPENDENTS_CPF_EMERGPHONE = API_URL + "/api/dependent/commonuser/verifyDependentsCPFandEmergPhone/params"
